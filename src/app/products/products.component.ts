64bec6c1659eb9171cae4a6d51c9edcaf41ac2f5
sruthi109 <sruthi.podapati@gmail.com>
2022-01-26 21:01:26 -0500

    users and products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -6,9 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  productDetails:any;
+  
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.productDetails =[
+       {
+      producturl:'laptop',
+      productdescription:'agfhfjkas',
+      cost:'50000'
+      },
+      {
+      producturl:'BlueTooth head set',
+      productdescription:'tygutyui',
+      cost:'2000'
+    },
+    {
+      producturl:'Airpods',
+      productdescription:'rdtfyguhirytyui',
+      cost:'15000'
+    },
+    {
 
+      producturl:'speakers',
+      productdescription:'esrdytfyuuhji',
+      cost:20000
+    },
+    {
+        producturl:'Television',
+      productdescription:'etrtyu',
+      cost:'80000'
+    },
+    {
+      producturl:'Refridgerator',
+      productdescription:'rftgyhurt',
+      cost:'60000'
+    },
+     {
+      producturl:'Air Conditioner',
+      productdescription:'edrftgyuhi',
+      cost:'45000'
+    },
+    {
+      producturl:'Water Purifier',
+      productdescription:'wertyu',
+      cost:'30000'
+    },
+    {
+      producturl:'Washing Machine',
+      productdescription:'ertyu',
+      cost:'60000'
+    },
+    {
+      producturl:'Dishwasher',
+      productdescription:'ertyu',
+      cost:'75000'
+    }
+   ]
+  }
   ngOnInit(): void {
   }
 
